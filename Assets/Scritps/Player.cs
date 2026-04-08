134a419bd75b99f8f18547bb2d3f74e4e4853140
fywww4 <a0900518266@gmail.com>
2026-04-08 16:28:39 +0800

    統神端火鍋下樓梯完成

--- Assets/Scritps/Player.cs
+++ Assets/Scritps/Player.cs
@@ -1,42 +1,141 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 5.0f;
-
+    [SerializeField] int Hp = 10;
+    [SerializeField] GameObject HpBar;
+    [SerializeField] Text scoreText;
+    int score = 0;
+    float scoreTime = 0;
     GameObject currentFloor;
+
+    AudioSource deathSound;
+    [SerializeField] GameObject replayButton;
+
+    void Start()
+    {
+        deathSound = GetComponent<AudioSource>();
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.RightArrow)) transform.Translate(moveSpeed * Time.deltaTime,0 ,0);
-        if (Input.GetKeyDown(KeyCode.LeftArrow)) transform.Translate(-moveSpeed * Time.deltaTime, 0, 0);
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.Translate(moveSpeed * Time.deltaTime, 0, 0);
+            GetComponent<SpriteRenderer>().flipX = false;
+            GetComponent<Animator>().SetBool("Run", true);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.Translate(-moveSpeed * Time.deltaTime, 0, 0);
+            GetComponent<SpriteRenderer>().flipX = true;
+            GetComponent<Animator>().SetBool("Run", true);
+        }
+        else
+        {
+            GetComponent<Animator>().SetBool("Run", false);
+        }
+
+        UpdatScore();
     }
 
     void OnCollisionEnter2D(Collision2D other)
     {
-        if(other.gameObject.tag == "Normal")
+        if (other.gameObject.tag == "Normal")
         {
             if (other.contacts[0].normal == new Vector2(0, 1.0f))
             {
                 Debug.Log("撞到第一種階梯");
                 currentFloor = other.gameObject;
+                ModifyHp(1);
+                other.gameObject.GetComponent<AudioSource>().Play();
             }
-        }   
+        }
         if (other.gameObject.tag == "Nails")
         {
             if (other.contacts[0].normal == new Vector2(0, 1.0f))
             {
                 Debug.Log("撞到第二種階梯");
                 currentFloor = other.gameObject;
+                ModifyHp(-3);
+                GetComponent<Animator>().SetTrigger("hurt");
+                other.gameObject.GetComponent<AudioSource>().Play();
             }
         }
         if (other.gameObject.tag == "Ceiling")
         {
             Debug.Log("碰到天花板了");
             currentFloor.GetComponent<BoxCollider2D>().enabled = false;
+            ModifyHp(-3);
+            GetComponent<Animator>().SetTrigger("hurt");
+            other.gameObject.GetComponent <AudioSource>().Play();
         }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "DeathLine")
+        {
+            Debug.Log("你輸了");
+            deathSound.Play();
+            Time.timeScale = 0.0f;
+            replayButton.SetActive(true);
+        }
+    }
+
+    void ModifyHp(int num)
+    {
+        Hp += num;
+        if (Hp > 10)
+        {
+            Hp = 10;
+        }
+        else if (Hp <= 0)
+        {
+            Hp = 0;
+            deathSound.Play();
+            Time.timeScale = 0.0f;
+            replayButton.SetActive(true);
+        }
+        UpdateHpBar();
+    }
+
+    void UpdateHpBar()
+    {
+        for (int i = 0; i < HpBar.transform.childCount; i++)
+        {
+            if (Hp > i)
+            {
+                HpBar.transform.GetChild(i).gameObject.SetActive(true);
+            }
+            else
+            {
+                HpBar.transform.GetChild(i).gameObject.SetActive(false);
+            }
+        }
+    }
+
+    void UpdatScore()
+    {
+        scoreTime += Time.deltaTime;
+        if (scoreTime > 2.0f)
+        {
+            score++;
+            scoreTime = 0;
+            scoreText.text = "地下" + score.ToString() + "層";
+        }
+    }
+
+    public void Replay()
+    {
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene("SampleScene");
     }
 }
